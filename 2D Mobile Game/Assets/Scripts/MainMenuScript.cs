38710ddbb8bd0a2d92ff3d5dfa8023917343f1bd
AR-Reynolds <24.alexander.reynold@ksd.org>
2024-01-23 13:54:51 -0800

    Added main menu script
    Fixed visual studio so there's a functioning main menu now.

---
+++ 2D Mobile Game/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public int difficulty;
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void PlayGame()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    public void ChangeDifficulty()
+    {
+        difficulty += 1;
+        if(difficulty > 3)
+        {
+            difficulty = 1;
+        }
+        PlayerPrefs.SetInt("Difficulty", difficulty);
+        Debug.Log(PlayerPrefs.GetInt("Difficulty"));
+    }
+}
