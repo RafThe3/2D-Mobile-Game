f9828f4aed5cded491b62b14ab7bc2eb1cbb9661
AR-Reynolds <24.alexander.reynold@ksd.org>
2024-01-24 13:54:31 -0800

    More work on vanities
    moved them into their own gameobject in the player for less clutter, also added regions & comments to the code for more clarity

--- 2D Mobile Game/Assets/Scripts/RoguelikeVanityTest.cs
+++ 2D Mobile Game/Assets/Scripts/RoguelikeVanityTest.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 
 public class RoguelikeVanityTest : MonoBehaviour
 {
+    //needs to be set to "true" otherwise vanities will never be visible, even if their respective bool is set to "true"
     public bool vanityEnabled;
 
+    //bools that check for object in question, if set to "true" will display vanity associated with that object
+    #region Vanity Bools
     public bool hasNecklace;
     public bool hasPropeller;
     public bool hasShield;
-    public GameObject necklace;
-    public GameObject propeller;
-    public GameObject shield;
+    public bool hasSmile;
+    #endregion
+
+    //gameobject variables for each vanity
+    #region Vanity GameObjects
+    GameObject necklace;
+    GameObject propeller;
+    GameObject shield;
+    GameObject smile;
+    #endregion
 
     void Start()
     {
+        //finds and sets respective gameobject variables to the associated GameObject
         vanityEnabled = false;
         necklace = GameObject.Find("NecklaceVanity");
         propeller = GameObject.Find("PropellerVanity");
         shield = GameObject.Find("ShieldVanity");
+        smile = GameObject.Find("SmileVanity");
     }
     void Update()
     {
+        //checks if the vanityEnabled bool is set to false. if it is, all vanities will NOT show
         if(!vanityEnabled)
         {
             necklace.GetComponent<SpriteRenderer>().enabled = false;
             propeller.GetComponent<SpriteRenderer>().enabled = false;
             shield.GetComponent<SpriteRenderer>().enabled = false;
+            smile.GetComponent<SpriteRenderer>().enabled = false;
         }
         else
         {
+            //bad code that just shows vanities if their respective bool is set to "true". if there's a better way to do this please let me know
             if(hasNecklace)
             {
                 necklace.GetComponent<SpriteRenderer>().enabled = true;
@@ -42,7 +57,10 @@ public class RoguelikeVanityTest : MonoBehaviour
             {
                 shield.GetComponent<SpriteRenderer>().enabled = true;
             }
-
+            if(hasSmile)
+            {
+                smile.GetComponent<SpriteRenderer>().enabled = true;
+            }
         }
     }
 }
