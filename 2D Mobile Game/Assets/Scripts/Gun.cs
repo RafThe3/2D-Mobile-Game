2df433f6b4886b521d6e505c03d5f73ae566cfab
Raf <25.rafael.gutierrez@ksd.org>
2024-01-29 12:53:39 -0800

    created joystick shooting and improved reloading

--- 2D Mobile Game/Assets/Scripts/Gun.cs
+++ 2D Mobile Game/Assets/Scripts/Gun.cs
@@ -12,7 +12,7 @@ public class Gun : MonoBehaviour
     [Min(0), SerializeField] private int damageToDeal = 1;
     [SerializeField] private bool infiniteAmmo = true;
     [Min(0), SerializeField] private int startingAmmo = 30;
-    [Min(0), SerializeField] private int numberOfRounds = 1;
+    [Min(0), SerializeField] private int startingRounds = 1;
 
     [Header("Bullet")]
     [SerializeField] private GameObject bulletPrefab;
@@ -23,6 +23,7 @@ public class Gun : MonoBehaviour
     [SerializeField] private AudioClip shootSFX;
     [SerializeField] private AudioClip reloadSFX;
     [SerializeField] private TMPro.TextMeshProUGUI ammoText;
+    [SerializeField] private Joystick aimingJoystick;
 
     //Internal Variables
     private float shootTimer;
@@ -32,7 +33,7 @@ public class Gun : MonoBehaviour
     {
         shootTimer = shootDelay;
         currentAmmo = startingAmmo;
-        reserveAmmo = (startingAmmo * numberOfRounds) - startingAmmo;
+        reserveAmmo = (startingAmmo * startingRounds) - startingAmmo;
         bulletPrefab.GetComponent<Bullet>().damage = damageToDeal;
     }
 
@@ -45,17 +46,27 @@ public class Gun : MonoBehaviour
 
         if (canShoot)
         {
-            bool isShooting = Input.GetButtonDown("Fire1") && !automaticFire || Input.GetButton("Fire1") && automaticFire;
-            if (isShooting && shootTimer >= shootDelay)
+            if (!FindObjectOfType<Player>().allowKeyControls)
             {
-                Shoot();
+                if (shootTimer >= shootDelay)
+                {
+                    JoystickShoot();
+                }
             }
+            else
+            {
+                bool isShooting = Input.GetButtonDown("Fire1") && !automaticFire || Input.GetButton("Fire1") && automaticFire;
+                if (isShooting && shootTimer >= shootDelay)
+                {
+                    Shoot();
+                }
+            }
+
 
             if (Input.GetKeyDown(KeyCode.R))
             {
                 Reload();
             }
-
         }
     }
 
@@ -92,8 +103,10 @@ public class Gun : MonoBehaviour
         {
             if (currentAmmo < startingAmmo && reserveAmmo > 0)
             {
-                reserveAmmo -= startingAmmo - currentAmmo;
-                currentAmmo = startingAmmo;
+                int reloadAmount = startingAmmo - currentAmmo;
+                reloadAmount = (reserveAmmo - reloadAmount) < 0 ? reserveAmmo : reloadAmount;
+                currentAmmo += reloadAmount;
+                reserveAmmo -= reloadAmount;
             }
         }
         Camera.main.GetComponent<AudioSource>().PlayOneShot(reloadSFX);
@@ -109,11 +122,33 @@ public class Gun : MonoBehaviour
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             Vector3 shootDir = mousePosition - transform.position;
             shootDir.Normalize();
-            bullet.GetComponent<Rigidbody2D>().velocity = 10 * bulletSpeed * shootDir;
+            float moveMultiplier = 10 * bulletSpeed;
+            bullet.GetComponent<Rigidbody2D>().velocity = moveMultiplier * shootDir;
             Camera.main.GetComponent<AudioSource>().PlayOneShot(shootSFX);
             Destroy(bullet, bulletLifeTime);
-            shootTimer = 0;
             currentAmmo--;
+            shootTimer = 0;
+        }
+    }
+
+    public void JoystickShoot()
+    {
+        Vector2 aim = Vector2.zero;
+        aim.x = aimingJoystick.Horizontal;
+        aim.y = aimingJoystick.Vertical;
+        bool isAiming = Mathf.Abs(aim.x) > 0 && Mathf.Abs(aim.y) > 0;
+
+        if (currentAmmo > 0 && isAiming)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Vector3 shootDir = new(aim.x, aim.y);
+            shootDir.Normalize();
+            float moveMultiplier = 10 * bulletSpeed;
+            bullet.GetComponent<Rigidbody2D>().velocity = moveMultiplier * shootDir;
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(shootSFX);
+            Destroy(bullet, bulletLifeTime);
+            currentAmmo--;
+            shootTimer = 0;
         }
     }
 }
