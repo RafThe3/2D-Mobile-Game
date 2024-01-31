3bc301c33696919bed3e03adb54054d2bc6bd28f
Raf <25.rafael.gutierrez@ksd.org>
2024-01-31 11:51:24 -0800

    Update MainMenuScript.cs

--- 2D Mobile Game/Assets/Scripts/MainMenuScript.cs
+++ 2D Mobile Game/Assets/Scripts/MainMenuScript.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public int difficulty;
+    [SerializeField] private int difficulty;
+    [SerializeField] private TMPro.TextMeshProUGUI difficultyText;
+
+    private void Start()
+    {
+        difficulty = 1;
+    }
+
+    private void Update()
+    {
+        UpdateDifficultyText();
+    }
+
+    private void UpdateDifficultyText()
+    {
+        difficultyText.text = difficulty == 1 ? "Change Difficulty - Easy"
+                    : difficulty == 2 ? "Change Difficulty - Normal"
+                    : "Change Difficulty - Hard";
+    }
+
     public void ExitGame()
     {
-        Debug.Log("Quitting Game");
+        Debug.Log("Quitting");
         Application.Quit();
     }
     public void PlayGame()
