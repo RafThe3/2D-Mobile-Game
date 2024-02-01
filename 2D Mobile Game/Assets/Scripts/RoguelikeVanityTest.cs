e146fb14f015eefdceb35c1a44cc65a8602063c2
AR-Reynolds <24.alexander.reynold@ksd.org>
2024-02-01 13:00:46 -0800

    changes to vanity test script and tags
    added vanity tag if we're going for that

--- 2D Mobile Game/Assets/Scripts/RoguelikeVanityTest.cs
+++ 2D Mobile Game/Assets/Scripts/RoguelikeVanityTest.cs
@@ -5,62 +5,34 @@ using UnityEngine;
 public class RoguelikeVanityTest : MonoBehaviour
 {
     //needs to be set to "true" otherwise vanities will never be visible, even if their respective bool is set to "true"
-    public bool vanityEnabled;
+    [SerializeField] private bool vanityEnabled;
+    [Min(0), SerializeField] private int vanityNumber;
 
-    //bools that check for object in question, if set to "true" will display vanity associated with that object
-    #region Vanity Bools
-    public bool hasNecklace;
-    public bool hasPropeller;
-    public bool hasShield;
-    public bool hasSmile;
-    #endregion
-
-    //gameobject variables for each vanity
-    #region Vanity GameObjects
-    GameObject necklace;
-    GameObject propeller;
-    GameObject shield;
-    GameObject smile;
-    #endregion
+    [SerializeField] private GameObject[] vanity;
 
     void Start()
     {
-        //finds and sets respective gameobject variables to the associated GameObject
-        vanityEnabled = false;
-        necklace = GameObject.Find("NecklaceVanity");
-        propeller = GameObject.Find("PropellerVanity");
-        shield = GameObject.Find("ShieldVanity");
-        smile = GameObject.Find("SmileVanity");
+       vanity = GameObject.FindGameObjectsWithTag("Vanity");
     }
+
     void Update()
     {
-        //checks if the vanityEnabled bool is set to false. if it is, all vanities will NOT show
-        if(!vanityEnabled)
+        UpdateVanity();
+    }
+
+    private void UpdateVanity()
+    {
+        if (!vanityEnabled)
         {
-            necklace.GetComponent<SpriteRenderer>().enabled = false;
-            propeller.GetComponent<SpriteRenderer>().enabled = false;
-            shield.GetComponent<SpriteRenderer>().enabled = false;
-            smile.GetComponent<SpriteRenderer>().enabled = false;
+            return;
         }
         else
         {
-            //bad code that just shows vanities if their respective bool is set to "true". if there's a better way to do this please let me know
-            if(hasNecklace)
-            {
-                necklace.GetComponent<SpriteRenderer>().enabled = true;
-            }
-            if (hasPropeller)
-            {
-                propeller.GetComponent<SpriteRenderer>().enabled = true;
-            }
-            if (hasShield)
-            {
-                shield.GetComponent<SpriteRenderer>().enabled = true;
-            }
-            if(hasSmile)
-            {
-                smile.GetComponent<SpriteRenderer>().enabled = true;
-            }
+            vanity[vanityNumber].GetComponent<SpriteRenderer>().enabled = vanityNumber == 1 ? vanity[1].GetComponent<SpriteRenderer>().enabled = true
+                : vanityNumber == 2 ? vanity[2].GetComponent<SpriteRenderer>().enabled = true
+                : vanityNumber == 3 ? vanity[2].GetComponent<SpriteRenderer>().enabled = true
+                : vanityNumber == 4 ? vanity[2].GetComponent<SpriteRenderer>().enabled = true
+                : true;
         }
     }
 }
