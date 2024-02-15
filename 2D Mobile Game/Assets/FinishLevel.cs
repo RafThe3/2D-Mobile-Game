5ee29e6a1e0a90383d7a228b63220d92485ff048
Raf <25.rafael.gutierrez@ksd.org>
2024-02-15 11:38:18 -0800

    changes to level 2

--- 2D Mobile Game/Assets/FinishLevel.cs
+++ 2D Mobile Game/Assets/FinishLevel.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class FinishLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameManager gameManager;
+    private EnemyCounter enemyCounter;
+
+    private void Awake()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
+        enemyCounter = FindObjectOfType<EnemyCounter>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player") && enemyCounter.enemiesRemaining <= 0)
+        {
+            gameManager.LoadNextScene();
+        }
     }
 }
